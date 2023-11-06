ac5fed0c5810e4934da9106500e22291b4cde671
Al Wakelin <al.wakelin@ntlworld.com>
2023-11-06 16:24:23 +0100

    copy the useful files to geoip2

--- example.py
+++ example.py
@@ -13,5 +13,16 @@ for file in os.listdir('zipped_data'):
     elif file.endswith('.tar.gz'):
         os.system(f'tar -k -xzf {zipped_data_folder}/{file} -C data')
 
+# For each folder in data, we copy the .mmdb files to the geoip2 folder.
 
+for folder in os.listdir('data'):
+    # check it is a folder nota a file
+    if not os.path.isdir(f'data/{folder}'):
+        continue
+
+    print(f'Handling {folder}...')
+    for file in os.listdir(f'data/{folder}'):
+        print(f'Handling {file}...')
+        if file.endswith('.mmdb'):
+            os.system(f'cp data/{folder}/{file} geoip2')
 
